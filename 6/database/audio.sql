8c9190cee1ceab8a8253501d4a7a841c3c4d09ce
Veirt <vveirt@gmail.com>
2023-10-22 15:40:41 +0800

    chore: upload images and database

---
+++ 6/database/audio.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 22, 2023 at 09:40 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `audio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `type` enum('IEM','Headphone','TWS','USB Dongles') NOT NULL,
+  `price` int(11) NOT NULL,
+  `image_path` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `type`, `price`, `image_path`) VALUES
+(1, 'MOONDROP Beautiful Worlds', 'IEM', 11791000, 'assets/uploads/2023-10-22 MOONDROP Beautiful Worlds.png'),
+(2, 'Tanchjim Hana', 'IEM', 2499000, 'assets/uploads/2023-10-22 Tanchjim Hana.png'),
+(3, 'Truthear Hola', 'IEM', 325000, 'assets/uploads/2023-10-22 Truthear Hola.png'),
+(4, 'MOONDROP Dawn Pro', 'USB Dongles', 768000, 'assets/uploads/2023-10-22 MOONDROP Dawn Pro.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
